98d04cc5c93440afd1fc71cd29c39f2fcda5f7bf
Evian <66403230+pku-zhb@users.noreply.github.com>
2022-07-17 00:29:14 +0800

    Add files via upload

---
+++ notion-automation/index.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv').config()
+
+const { Client } = require("@notionhq/client")
+
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+})
+
+
+const result = (async () => {
+    const databaseId = process.env.NOTION_DATABASE_ID;
+    const response = await notion.databases.retrieve({ database_id: databaseId });
+    console.log(response);
+  })();
+
